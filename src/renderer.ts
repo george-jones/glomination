f9b9d7f9fa6e55ea1fe956c5498d3cf6ca86c0ca
George Jones <gjones@mieweb.com>
2020-08-07 01:13:06 -0400

    Camera motions are pretty good.  Zooming is still a little weirdish, but hey, good enough for now.

--- src/renderer.ts
+++ src/renderer.ts
@@ -6,6 +6,9 @@ export default class Renderer {
 	private _scene: BABYLON.Scene;
 
 	createScene(canvas: HTMLCanvasElement, engine: BABYLON.Engine) {
+		const lightDistance = 10;
+		const minWheelPrecision = 5;
+		const maxWheelPrecision = 250;
 		this._canvas = canvas;
 
 		this._engine = engine;
@@ -14,28 +17,51 @@ export default class Renderer {
 		const scene = new BABYLON.Scene(engine);
 		this._scene = scene;
 
-		// This creates and positions a free camera (non-mesh)
-		const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 0, -2.65), scene);
+		scene.clearColor = new BABYLON.Color4(0.1, 0.1, 0.1, 1.0);
 
-		// This targets the camera to scene origin
-		camera.setTarget(BABYLON.Vector3.Zero());
+		// This creates and positions a free camera (non-mesh)
+		//const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 0, -2.65), scene);
+		const camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI/2, Math.PI/2, 2.65, BABYLON.Vector3.Zero(), scene);
+		camera.lowerRadiusLimit = 2.0;
+		camera.upperRadiusLimit = 50;
+		camera.wheelPrecision = 250;
+		camera.angularSensibilityX = 2000;
+		camera.angularSensibilityY = 2000;
+		camera.attachControl(canvas, false)
 
 		// This attaches the camera to the canvas
-		camera.attachControl(canvas, true);
+		//camera.attachControl(canvas, true);
 
 		// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-		const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 0, -10), scene);
+		//const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0.5, 0.5, -1), scene);
+		//const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 0, -1), scene);
+		const light = new BABYLON.PointLight("sun", new BABYLON.Vector3(0, 0, -1 * lightDistance), scene);
 
 		// Default intensity is 1. Let's dim the light a small amount
-		light.intensity = 0.7;
+		light.intensity = 0.75; //0.7;
 
 		const materialForBall = new BABYLON.StandardMaterial("texture1", scene);
-		materialForBall.wireframe = true;
+		materialForBall.specularColor = new BABYLON.Color3(0, 0, 0); // no shininess
+		///materialForBall.wireframe = true;
+
+/*
+	 * 'Picasso (very low)' -> 1280 faces --> 8 subdiv
+	 * 'Low' -> 5120 faces --> 16 subdiv
+	 * 'Medium' -> 20480 faces --> 32 subdiv
+	 * 'High' -> 81920 faces --> 64 subdiv
+*/
 
 		// Our built-in 'sphere' shape. Params: name, subdivs, size, scene
 		//const sphere = BABYLON.Mesh.CreateSphere("sphere1", 32, 1, scene);
-		const sphere = BABYLON.MeshBuilder.CreateIcoSphere("globe", {radius: 1, radiusY: 1, subdivisions: 20}, scene);
+		const sphere = BABYLON.MeshBuilder.CreateIcoSphere("globe", {radius: 1, subdivisions: 64 }, scene);
 		sphere.material = materialForBall;
+
+		sphere.updateFacetData();
+		console.log('Number of facets', sphere.facetNb);
+
+		// This targets the camera to scene origin
+		camera.setTarget(sphere);
+
 
 		// Move the sphere upward 1/2 its height
 		//sphere.position.y = 1;
@@ -48,8 +74,23 @@ export default class Renderer {
 		*/
 		//sphere.rotate(BABYLON.Axis.X, -Math.PI/2, BABYLON.Space.WORLD);
 
-		// Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-		//const ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+		let lastCameraPos = camera.position.clone();
+		scene.onBeforeRenderObservable.add(function () {
+			if (lastCameraPos.x != camera.position.x ||
+				lastCameraPos.y != camera.position.y ||
+				lastCameraPos.z != camera.position.z)
+			{
+				let lightPos = camera.position.clone();
+				lightPos.scaleInPlace(lightDistance / lightPos.length());
+
+				light.position = lightPos;
+
+				lastCameraPos = camera.position.clone();
+
+				camera.wheelPrecision = Math.max(minWheelPrecision, maxWheelPrecision - 100 * (lastCameraPos.length() - camera.lowerRadiusLimit));
+				//console.log(camera.wheelPrecision);
+			}
+		});
 	}
 
 	initialize(canvas: HTMLCanvasElement) {
