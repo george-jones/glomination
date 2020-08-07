dc8ee93d45a934d7424fd0338fc4e9ec86eb5303
George Jones <gjones@mieweb.com>
2020-08-06 23:23:45 -0400

    World is no an icosphere.  I don't know how the number of subdivisions compares to what I made in three.js but it seems like I can make an insane number of triangles without killing my PC.

--- src/renderer.ts
+++ src/renderer.ts
@@ -15,7 +15,7 @@ export default class Renderer {
 		this._scene = scene;
 
 		// This creates and positions a free camera (non-mesh)
-		const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+		const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 0, -2.65), scene);
 
 		// This targets the camera to scene origin
 		camera.setTarget(BABYLON.Vector3.Zero());
@@ -24,19 +24,32 @@ export default class Renderer {
 		camera.attachControl(canvas, true);
 
 		// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-		const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+		const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 0, -10), scene);
 
 		// Default intensity is 1. Let's dim the light a small amount
 		light.intensity = 0.7;
 
+		const materialForBall = new BABYLON.StandardMaterial("texture1", scene);
+		materialForBall.wireframe = true;
+
 		// Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-		const sphere = BABYLON.Mesh.CreateSphere("sphere1", 5, 2, scene);
+		//const sphere = BABYLON.Mesh.CreateSphere("sphere1", 32, 1, scene);
+		const sphere = BABYLON.MeshBuilder.CreateIcoSphere("globe", {radius: 1, radiusY: 1, subdivisions: 20}, scene);
+		sphere.material = materialForBall;
 
 		// Move the sphere upward 1/2 its height
-		sphere.position.y = 1;
+		//sphere.position.y = 1;
+
+		/*
+		let axis = new BABYLON.Vector3(1, 0, 0);
+		let angle = Math.PI / 2;
+		let quaternion = new BABYLON.Quaternion.RotationAxis(axis, angle);
+		sphere.rotationQuaternion = quaternion;
+		*/
+		//sphere.rotate(BABYLON.Axis.X, -Math.PI/2, BABYLON.Space.WORLD);
 
 		// Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-		const ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+		//const ground = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
 	}
 
 	initialize(canvas: HTMLCanvasElement) {
